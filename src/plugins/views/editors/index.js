02b50b720004afb4e2a0b0ed35688495ce5f88f5
Manuel Silva <manuel.silva@mov.ai>
2021-12-27 03:43:37 +0000

    Add flow to docs

---
+++ src/plugins/views/editors/index.js
@@ -0,0 +1,6 @@
+import Configuration from "./Configuration/Configuration";
+import Callback from "./Callback/Callback";
+import Node from "./Node/Node";
+import Flow from "./Flow/FlowPlugin";
+
+export { Configuration, Callback, Node, Flow };
