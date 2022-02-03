3c61e84775eb9aae13deb7f757f6176f964b8bb2
Manuel Nogueira <manuel.nogueira@mov.ai>
2022-02-03 18:12:52 +0000

    Added temporary Main Menu button for HomeTab

--- src/plugins/views/MainMenu/MainMenu.jsx
+++ src/plugins/views/MainMenu/MainMenu.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { useTranslation } from "react-i18next";
 import PropTypes from "prop-types";
 import { withViewPlugin } from "../../../engine/ReactPlugin/ViewReactPlugin";
 import {
@@ -7,14 +8,15 @@ import {
   ContextMenu
 } from "@mov-ai/mov-fe-lib-react";
 import { Authentication } from "@mov-ai/mov-fe-lib-core";
+import HomeIcon from "@material-ui/icons/Home";
 import TextSnippetIcon from "@material-ui/icons/Description";
 import AppsIcon from "@material-ui/icons/Apps";
 import AddBoxIcon from "@material-ui/icons/AddBox";
 import { Tooltip } from "@material-ui/core";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import { MainContext } from "../../../main-context";
-import { VERSION } from "../../../utils/Constants";
-import { getIconByScope } from "../../../utils/Utils";
+import { HOMETAB_PROFILE, VERSION } from "../../../utils/Constants";
+import { getIconByScope, getHomeTab } from "../../../utils/Utils";
 
 const useStyles = makeStyles(theme => ({
   icon: {
@@ -26,53 +28,66 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-const MENUS = [
-  {
-    name: "explorer",
-    icon: props => <TextSnippetIcon {...props}></TextSnippetIcon>,
-    title: "Explorer",
-    isActive: true,
-    getOnClick: (call, emit) => () => {
-      // Toggle left drawer
-      call("leftDrawer", "toggle");
-    }
-  }
-  // {
-  //   name: "fleet",
-  //   icon: props => <AndroidIcon {...props}></AndroidIcon>,
-  //   title: "Fleet",
-  //   getOnClick: (call, emit) => () => {
-  //     // TODO: Open Fleet tab
-  //     console.log("debug open Fleet");
-  //   }
-  // },
-  // {
-  //   name: "debug",
-  //   icon: props => <BugReportIcon {...props}></BugReportIcon>,
-  //   title: "Debug",
-  //   getOnClick: (call, emit) => () => {
-  //     // TODO: Open Debug options
-  //     console.log("debug open Debug");
-  //   }
-  // },
-  // {
-  //   name: "diff",
-  //   icon: props => <CompareIcon {...props}></CompareIcon>,
-  //   title: "Diff tool",
-  //   getOnClick: (call, emit) => () => {
-  //     // TODO: Open DiffTool
-  //     console.log("debug open Diff Tool");
-  //   }
-  // }
-];
-
 const MainMenu = props => {
   const { call, emit } = props;
-  const [docTypes, setDocTypes] = React.useState([]);
+  // State hooks
+  const [docTypes, setDocTypes] = useState([]);
+  // Other hooks
   const classes = useStyles();
   const theme = useTheme();
+  const { t } = useTranslation();
+  const MENUS = useRef([
+    {
+      name: HOMETAB_PROFILE.name,
+      icon: props => <HomeIcon {...props}></HomeIcon>,
+      title: t("Reopen Welcome Tab"),
+      isActive: true,
+      getOnClick: (call, emit) => {
+        getHomeTab().then(homeTab => {
+          call("tabs", "open", homeTab);
+        });
+      }
+    },
+    {
+      name: "explorer",
+      icon: props => <TextSnippetIcon {...props}></TextSnippetIcon>,
+      title: "Explorer",
+      isActive: true,
+      getOnClick: (call, emit) => {
+        // Toggle left drawer
+        call("leftDrawer", "toggle");
+      }
+    }
+    // {
+    //   name: "fleet",
+    //   icon: props => <AndroidIcon {...props}></AndroidIcon>,
+    //   title: "Fleet",
+    //   getOnClick: (call, emit) => () => {
+    //     // TODO: Open Fleet tab
+    //     console.log("debug open Fleet");
+    //   }
+    // },
+    // {
+    //   name: "debug",
+    //   icon: props => <BugReportIcon {...props}></BugReportIcon>,
+    //   title: "Debug",
+    //   getOnClick: (call, emit) => () => {
+    //     // TODO: Open Debug options
+    //     console.log("debug open Debug");
+    //   }
+    // },
+    // {
+    //   name: "diff",
+    //   icon: props => <CompareIcon {...props}></CompareIcon>,
+    //   title: "Diff tool",
+    //   getOnClick: (call, emit) => () => {
+    //     // TODO: Open DiffTool
+    //     console.log("debug open Diff Tool");
+    //   }
+    // }
+  ]);
 
-  React.useEffect(() => {
+  useEffect(() => {
     call("docManager", "getDocTypes").then(_docTypes => {
       setDocTypes(_docTypes);
     });
@@ -125,11 +140,11 @@ const MainMenu = props => {
               }))}
             ></ContextMenu>
           }
-          navigationList={MENUS.map(menu => (
+          navigationList={MENUS.current.map(menu => (
             <Tooltip title={menu.title} placement="right" arrow>
               {menu.icon({
                 className: classes.icon,
-                onClick: menu.getOnClick(call, emit)
+                onClick: () => menu.getOnClick(call, emit)
               })}
             </Tooltip>
           ))}
