962e1b86df65a21592c5fc17011b973c5e7f4dc1
PedroCristovao-Movai <pedro.cristovao@mov.ai>
2022-04-01 10:26:54 +0000

    minor clean up

--- config-overrides.js
+++ config-overrides.js
@@ -1,52 +1,23 @@
-const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
-const nodeExternals = require('webpack-node-externals');
-const path = require("path");
+const merge = require("lodash.merge");
 
 module.exports = function override(config, env) {
-  // Add alias to vscode
-  config.resolve.alias.vscode = require.resolve(
-    "@codingame/monaco-languageclient/lib/vscode-compatibility"
-  );
-  // target
-  config.target = "web";
-  config.entry = "./src/index.js"
-  // Add plugins
-  config.plugins = [...config.plugins, new MonacoWebpackPlugin()];
-  config.externals = [nodeExternals({importType: "umd"})]
-  config.output.libraryTarget = "amd";
-  config.output.library = "IDE_CE";
-  // Optimizations
-  config.optimization =  {
-    ...config.optimization,
-    concatenateModules: false,
-    providedExports: false,
-    usedExports: false,
-  }
-  // Add loaders
-  config.module.rules = [
-    ...config.module.rules,
-    {
-      test: /node_modules\/monaco-editor/,
-      use: {
-        loader: "babel-loader",
-        options: { presets: ["@babel/preset-env"] }
-      }
-    },
-    {
-      test: /.css$/,
-      resolve: {
-        extensions: [".css"]
+  const finalConfig = merge(config, {
+    resolve: {
+      alias: {
+        vscode: require.resolve(
+          "@codingame/monaco-languageclient/lib/vscode-compatibility"
+        )
       },
-      use: ["style-loader", "css-loader", "postcss-loader"],
-      include: [
-        path.resolve(__dirname, "./node_modules/monaco-editor")
-      ]
-    },
-    {
-      test: /\.ttf$/,
-      use: ['file-loader'],
-    },
-  ];
-  // return config
-  return config;
+      extensions: [".js", ".json", ".ttf"]
+    }
+  });
+
+  finalConfig.entry = {
+    main: finalConfig.entry,
+    "editor.worker": "monaco-editor-core/esm/vs/editor/editor.worker.js"
+  };
+
+  finalConfig.output.filename = "[name].bundle.js";
+
+  return finalConfig;
 };
