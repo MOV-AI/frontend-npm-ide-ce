edd95c2aa8a3b07bc036e940e456725c4c5adb5d
Rian Martins <rian.martins@mov.ai>
2022-04-14 17:26:24 +0000

    Adjust callback to python code guidelines

--- database/Callback/backend.DataValidation.py
+++ database/Callback/backend.DataValidation.py
@@ -8,7 +8,7 @@
 import re
 try:
     from dal.scopes import ScopesTree
-except:
+except ImportError:
     from movai.data import ScopesTree
 
 
@@ -26,12 +26,12 @@ def validate_configuration(config_string):
         matches_size = len([x for x in matches])
         if matches_size == 0:
             return False
-        configKeyPath = config_string.replace("$(config ","").replace(")", "").split(".")
-        configName = configKeyPath[0]
-        configKeyPath.pop(0)
-        config = ScopesTree()().Configuration[configName]
+        config_key_path = config_string.replace("$(config ","").replace(")", "").split(".")
+        config_name = config_key_path[0]
+        config_key_path.pop(0)
+        config = ScopesTree()().Configuration[config_name]
         val = config.get_value()
-        for key in configKeyPath:
+        for key in config_key_path:
             val = val[key]
         return True
     except Exception as e:
