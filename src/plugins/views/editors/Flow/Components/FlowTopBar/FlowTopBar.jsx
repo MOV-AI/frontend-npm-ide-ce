d0b5c2cd3e7edf4d8255b78b492c1167356333e9
Rian Martins <rian.martins@mov.ai>
2022-05-04 17:36:46 +0000

    FP-1749 : Select default robot if currently saved ID is not in the robot list

--- src/plugins/views/editors/Flow/Components/FlowTopBar/FlowTopBar.jsx
+++ src/plugins/views/editors/Flow/Components/FlowTopBar/FlowTopBar.jsx
@@ -150,7 +150,7 @@ const FlowTopBar = props => {
    * @param {String} currentRobot : current selected robot id
    */
   const getRunningRobot = useCallback(
-    currentRobot => {
+    (currentRobot, robots) => {
       // If there's a currently selected robot in local storage
       if (currentRobot) initSelectedRobot(currentRobot);
       // Call callback to get default robot
@@ -167,10 +167,14 @@ const FlowTopBar = props => {
               };
             });
             // Update Flow selected robot if none is selected yet
-            if (!currentRobot) {
+            let robotToSelect = currentRobot;
+            if (!currentRobot || !(currentRobot in robots)) {
               workspaceManager.setSelectedRobot(robotId);
               initSelectedRobot(robotId);
+              robotToSelect = robotId;
             }
+            // Set selected robot
+            setRobotSelected(robotToSelect);
           }
         })
         .catch(err => {
@@ -206,9 +210,8 @@ const FlowTopBar = props => {
       });
       // Update state hooks
       setRobotList(robots);
-      setRobotSelected(currentSelected);
       // Get running Robot
-      getRunningRobot(currentSelected);
+      getRunningRobot(currentSelected, robots);
     },
     [getRunningRobot, workspaceManager]
   );
