16d577bdb9fe3fea7fd7b71e7b8f6b4f8d4941da
Jose Sampaio <jose.sampaio@mov.ai>
2022-06-24 15:10:52 +0000

    Remove unused import

--- src/plugins/views/editors/Flow/Components/FlowTopBar/FlowSearch.jsx
+++ src/plugins/views/editors/Flow/Components/FlowTopBar/FlowSearch.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback } from "react";
 import SearchIcon from "@material-ui/icons/Search";
 import PropTypes from "prop-types";
 import {
